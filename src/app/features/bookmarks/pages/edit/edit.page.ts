cf4a2f8da567e76b9c3866958c20c1e696c6e155
Tudor Alexandru <alextd90@yahoo.com>
2025-10-21 20:41:29 +0300

    use reusable form in edit page

--- src/app/features/bookmarks/pages/edit/edit.page.ts
+++ src/app/features/bookmarks/pages/edit/edit.page.ts
@@ -1,30 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { AsyncPipe, NgIf } from '@angular/common';
 import { Store } from '@ngrx/store';
+import { Observable, map, switchMap, tap } from 'rxjs';
 
 import { Routes } from '../../../../_shared/routes';
 import { BookmarksService } from '../../../../api/bookmarks.service';
 import { Bookmark } from '../../../../models/bookmarks/bookmark';
-import { NgIf } from '@angular/common';
 import { getListAction } from '../../../../store/bookmarks';
+import { FormComponent } from '../../components/form/form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule,
-    NgIf],
+  imports: [AsyncPipe, NgIf, FormComponent, MatCardModule],
   templateUrl: './edit.page.html',
   styleUrl: './edit.page.scss',
 })
@@ -34,61 +24,25 @@ export class EditPage {
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly service = inject(BookmarksService);
 
-  private readonly fb = inject(FormBuilder);
-
-  form = this.fb.group({
-    title: ['', [Validators.required, Validators.minLength(2)]],
-    url: ['', [Validators.required]],
-  });
-
-  id: number | undefined;
-
-  bookmark!: Bookmark;
+  bookmark$!: Observable<Bookmark>;
 
   constructor() {
-    this.activatedRoute.params.subscribe((params) => {
-      if (params.hasOwnProperty('id')) {
-        this.id = params['id'];
-
-        this.getData();
-      } else {
-        this.router.navigateByUrl(Routes.bookmarks.root);
-      }
-    });
-  }
-  
-  get title() {
-    return this.form.controls.title;
-  }
-  get url() {
-    return this.form.controls.url;
+    this.bookmark$ = this.activatedRoute.params.pipe(
+      map((params) => params['id']),
+      tap((id) => {
+        if (!id) {
+          this.router.navigateByUrl(Routes.bookmarks.root);
+        }
+      }),
+      switchMap((id) => this.service.getById(id)),
+    );
   }
 
-  getData(): void {
-    this.service.getById(this.id!).subscribe(data => {
-      this.bookmark = data;
-      this.initForm();
-    })
-  }
-
-  initForm(): void {
-    this.form.setValue({
-      title: this.bookmark?.title || '',
-      url: this.bookmark?.url || ''
-    });
-  }
-
-  onSubmit(): void {
-    if (this.form.invalid) return;
-    const { title, url } = this.form.getRawValue();
-
-    this.bookmark.title = title!;
-    this.bookmark.url = url!;
-    
+  submit(bookmark: Bookmark): void {
     // old way - call directly the service (if there is not a defined store & effect to call it)
-    this.service.update(this.bookmark!).subscribe(() => {
-      this.store.dispatch(getListAction({keyword: ''}))
+    this.service.update(bookmark!).subscribe(() => {
+      this.store.dispatch(getListAction({ keyword: '' }));
       this.router.navigateByUrl(Routes.bookmarks.root);
-    })
+    });
   }
 }
